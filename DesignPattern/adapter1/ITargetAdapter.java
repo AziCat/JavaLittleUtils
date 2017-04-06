9752dab226a55e36ea38b07dfe578e3119b15f38
AziCat <1003980136@qq.com>
2017-04-06 09:59:54 +0800

    目录调整

---
+++ DesignPattern/adapter1/ITargetAdapter.java
@@ -0,0 +1,16 @@
+package yan.creator.adapter1;
+/**
+ * 适配器接口
+ * @author yanjunhao
+ *
+ */
+public interface ITargetAdapter {
+	/**
+	 * 目标类原方法
+	 */
+	public void sourceMethod();
+	/**
+	 * 扩展方法
+	 */
+	public void extendMethod();
+}
